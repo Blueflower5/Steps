c445c26977a7c7cdac0606bcabb3b950f5d5481f
Blueflower5 <mohammad9776@gmail.com>
2024-12-02 13:10:36 +0430

    begin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="steps">
+      Hello React
+      <div></div>
+      <div></div>
+      <div></div>
+      <p className="message">Hello</p>
+      <div className="buttons"></div>
+      <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        Previous
+      </button>
+      <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        Next
+      </button>
     </div>
   );
 }
-
-export default App;
